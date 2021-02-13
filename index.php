401986b05110bdafb25073a5e227d0aeb719eb49
Rishikesh Devanathan <rishi.devanathan@gmail.com>
2021-02-13 23:16:02 +0530

    Footer Completed

--- index.php
+++ index.php
@@ -14,7 +14,10 @@ session_start();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+    
     <link rel="stylesheet" href="./src/css/navbar.css">
+    <link rel="stylesheet" href="./src/css/footer.css">
 
     <!-- temporary preview of the style -->
     <style>
@@ -36,9 +39,11 @@ session_start();
         <?php include './partials/navbar.php' ?>
         <!-- preview text -->
         <h1>SAAR-IITP</h1>
+        
     </div>
 
     <?php include './partials/footer.php' ?>
+    
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,97 @@
-<!-- to make -->
+<!-- Footer -->
+<footer class="text-center text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!--Grid row-->
+        <div class="row">
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <img src="src/img/logo1.png" alt="white logo" width="131" height="90">
+                <p style="padding-top: 10px;">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                    molestias.
+                </p>
+                <h4 style="padding-top: 20px;">Follow Us!</h4>
+                <ul class="social-network social-circle">
+                    <li><a href="https://www.facebook.com/saar.iitp/" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/saar.iitp" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/saar-iitp" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://www.twitter.com/saar_iitp" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCoAb1YggffbfgVxQyXaZB-g" class="icoYoutube" title="Youtube"><i class="fa fa-youtube"></i></a></li>
+                </ul>
+            </div>
+            <!--Grid column-->
+
+
+            <!--Grid column-->
+            <div style="padding: 10px;  text-align: center;" class="col-lg-4 col-md-12 col-sm-12">
+
+                <!--Grid column-->
+                <div style="padding-left: 10px;" class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
+                    <h5 style="font-weight: 700; font-size: 15px;" class="text-uppercase">Quick Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="https://www.iitp.ac.in/" id="text-dark">IIT Patna</a>
+                        </li>
+                        <li>
+                            <a href="https://www.iitp.ac.in/alumni/" id="text-dark">ITPAA</a>
+                        </li>
+                        <li>
+                            <a href="http://saar.iitp.ac.in/#" id="text-dark">Reach Us</a>
+                        </li>
+                        <li>
+                            <a href="http://saar.iitp.ac.in/#" id="text-dark">Terms of Use</a>
+                        </li>
+                        <li>
+                            <a href="http://saar.iitp.ac.in/#" id="text-dark">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!--Grid column-->
+                <div style="padding-left: 10px;" class="col-lg-8 col-md-6 col-sm-6 col-xs-6">
+                    <h5 style="font-weight: 700; font-size: 15px;" class="text-uppercase">Contact Us</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <p id="text-dark">Office of A Dean Resource <br>
+                                Administrative Building, IIT Patna</p>
+                        </li>
+                        <li>
+                            <p id="text-dark">Prof. In-charge: <br>
+                                Dr. Anirban Chowdhary <br>
+                                Email - pic_alumni@iitp.ac.in</p>
+                        </li>
+                        <li>
+                            <p id="text-dark">General Secretary: <br>
+                                Gaurav Pratap Singh <br>
+                                Email - 1801ee63@iitp.ac.in</p>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                <div id="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14399.37835508978!2d84.8434447170254!3d25.54355318850096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398d567a193702ff%3A0xc9c527c7faec3056!2sIIT+Patna+Administration+Block!5e0!3m2!1sen!2sin!4v1539177184721" width="100%" height="225px" frameborder="0" style="border-radius: 5px" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <!--Grid column-->
+        </div>
+
+        <!-- Copyright -->
+        <div class="row">
+            <div class="col-md-12 copy">
+                <p class="text-center">&copy; || Copyright || IIT Patna SAAR Team ||</p>
+            </div>
+        </div>
+        <!-- Copyright -->
+        <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+
+</footer>
+<!-- Footer -->
