7b82f8d77fb2d01e684af8f460dc9d9b5123424e
Pratyush Kumar <40167899+StrixROX@users.noreply.github.com>
2021-03-21 00:03:12 +0530

    Clean Slate for Revamp 2

--- index.php
+++ index.php
@@ -10,10 +10,6 @@ session_start();
 
     <!-- preconnecting to google fonts to increase loading speed-->
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-
-    <!-- all style imports -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <!-- bootstrap styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" > <!-- fontawesome -->
     
     <!-- all script imports -->
     <script
@@ -21,74 +17,20 @@ session_start();
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous">
     </script> <!-- jquery -->
-    <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous">
-    </script><!-- popper.js -->
-    <script
-        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous">
-    </script><!-- bootstrap js -->
 
-    <!-- all third party imports have to be done before the custom imports -->
+    <!-- all third-party imports have to be done before the custom imports -->
 
     <!-- all custom style imports -->
     <?php include './partials/head.php' ?>
 
     <link rel="stylesheet" href="./src/css/navbar.css">
-    <link rel="stylesheet" href="./src/css/footer.css">
-    <link rel="stylesheet" href="./src/css/form.css">
-    
     <link rel="stylesheet" href="./src/css/home_base.css">
-
+    <link rel="stylesheet" href="./src/css/footer.css">
 
 </head>
 <body>
 
     <?php include './partials/navbar.php' ?>
-    
-    <section class="landing">
-        <div class="image-wrapper">
-            <div class="box"></div>
-            <img src="./src/img/slider/test.jpg">
-            <div class="box-b"></div>
-        </div>
-
-        <div class="logo">
-            <img src="./src/img/logo2.png">
-            <h5>IIT Patna</h5>
-            <h1>Students' Association for<br>Alumni Relations </h1>
-        </div>
-        <div class="scroll">›</div>
-    </section>
-
-    <section class="about">
-        <div class="content">
-            <h1>About Us</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus porro quisquam magnam molestiae perferendis, voluptates delectus, optio sed totam a dolorem corrupti pariatur! Nulla voluptate iusto in possimus dolor rerum perferendis consectetur itaque, at obcaecati aliquam. Illo libero omnis, blanditiis possimus ex, velit necessitatibus eaque quis aliquid assumenda vel voluptatem.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptas, est fugiat amet vitae optio eius! Alias ut harum, maxime adipisci molestias cum. Odio vitae, distinctio in inventore quos expedita!</p>
-        </div>
-    </section>
-
-    <section class="gallery">
-        <h1>Gallery</h1>
-        <div class="gallery_container">
-            <img class="image active" src="./src/img/slider/1.jpg" data-index="0">
-            <img class="image next" src="./src/img/slider/2.jpg" data-index="1">
-            <img class="image" src="./src/img/slider/3.jpg" data-index="2">
-            <img class="image" src="./src/img/slider/4.jpg" data-index="3">
-            <img class="image prev" src="./src/img/slider/5.jpg" data-index="4">
-        </div>
-    </section>
-
-    <section class="newsletter"> <!-- TODO -->
-        <h1>Newsletter</h1>
-        <!-- <div class="newsletter_container">
-            <a href="public/anusmriti/vol3/issue1.pdf" target="_blank"><img class="image active" src="./src/img/slider/1.jpg"></a>
-        </div> -->
-    </section>
 
     <?php include './partials/footer.php'?>
 
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,36 +1,3 @@
 <nav class="navbar">
-    <div class="container-fluid">
-      <div id="SAAR-logo-white" class="navbar-brand center-block" href="#">
-        <img src="src/img/logo1.png" alt="white logo" width="100" height="70">
-      </div>
 
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <svg class="ham hamRotate ham1" viewBox="0 0 100 100" width="50" onclick="this.classList.toggle('active')">
-            <path class="line top" d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-            <path class="line middle" d="m 30,50 h 40" />
-            <path class="line bottom" d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
-          </svg>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <div class="tab"><a href="#" class="hoverable"> Home</a></div>
-          </li>
-          <li>
-            <div class="tab"><a href="#" class="hoverable"> About Us</a></div>
-          </li>
-          <li>
-            <div class="tab"><a href="#" class="hoverable"> Gallery</a></div>
-          </li>
-          <li>
-            <div class="tab"><a href="#" class="hoverable"> Timeline</a></div>
-          </li>
-          <li>
-            <div class="tab"><a href="#" class="hoverable"> Contact</a></div>
-          </li>
-        </ul>
-      </div>
-    </div>
 </nav>
