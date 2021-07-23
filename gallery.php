146ec761ea4940f65b854655e00938228f4e2a73
Rishikesh Devanathan <rishi.devanathan@gmail.com>
2021-07-23 12:19:39 +0530

    Gallery with updated design

--- gallery.php
+++ gallery.php
@@ -20,31 +20,45 @@
 
 <body>
 
-    <img src="./src/assets/images/cubes.svg" class="cubes top-left">
-    <img src="./src/assets/images/cubes.svg" class="cubes bottom-right">
+    <img src="./src/assets/images/cubes.svg" class="cubes top-right">
+    <img src="./src/assets/images/cubes.svg" class="cubes bottom-left">
 
     <div class="header">
-        <img src="./src/assets/images/back-head-white.svg" class="back-button" href="./index.php">
+        <a class="back-button" href="./index.php">
+            <img src="./src/assets/images/back-head-white.svg">
+        </a>
+
         <img src="./src/assets/images/logo1.png" class="logo">
     </div>
 
     <div class="viewer">
-        <img src="./src/assets/images/gallery-images/dc.jpg" class="active-img">
+
+        <img class="album-image prev">
+        <img class="album-image active">
+        <img class="album-image next">
+
     </div>
 
-    <div class="scroller">
-        <img src="./src/assets/images/gallery-images/dc.jpg" class="image-card active">
-        <img src="./src/assets/images/gallery-images/gconvo.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/bconvo.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/sconvo.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/cs.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/ee.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/mech.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/btech14-18ch.jpg" class="image-card">
-        <img src="./src/assets/images/gallery-images/btech14-18ce.jpg" class="image-card">
+    <img src="./src/assets/images/back-head-white.svg" class="arrow-icon hide" href="./index.php">
+
+    <div class="scroller hide">
+        <div class="album active">
+            <img src="./src/assets/images/gallery-images/dc.jpg" class="image-card album-cover">
+            <img src="./src/assets/images/gallery-images/gconvo.jpg" class="image-card">
+            <img src="./src/assets/images/gallery-images/bconvo.jpg" class="image-card">
+            <img src="./src/assets/images/gallery-images/sconvo.jpg" class="image-card">
+            <h3 class="album-title">Graduation Ceremony</h3>
+        </div>
+        <div class="album">
+            <img src="./src/assets/images/gallery-images/cs.jpg" class="image-card album-cover">
+            <img src="./src/assets/images/gallery-images/ee.jpg" class="image-card">
+            <img src="./src/assets/images/gallery-images/mech.jpg" class="image-card">
+            <img src="./src/assets/images/gallery-images/btech14-18ch.jpg" class="image-card">
+            <img src="./src/assets/images/gallery-images/btech14-18ce.jpg" class="image-card">
+            <h3 class="album-title">Batch Photos '18</h3>
+        </div>
     </div>
 
-    <?php include getcwd() . '/partials/footer.php' ?>
     <script src="src/js/gallery.js"></script>
 
 </body>
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,17 +1,75 @@
 $('body').ready(() => {
 
+    $('.scroller .album').each((i, el) => {
+        let album_cover = $(el).children()[0];
+        el.style.backgroundImage = 'url(' + album_cover.src + ')';
+    });
+
+    let album_images = {};
+    $('.scroller .album.active .image-card').each((i, el) => {
+        album_images[i] = el;
+    });
+
+    let num = Object.keys(album_images).length;
+    let prev = num - 1;
+    let active = 0;
+    let next = 1;
+
+    init_carousel(prev, active, next);
     slider();
+    pull_up();
+
+
+    function init_carousel(p, a, n) {
+        // $('.viewer .album-image').removeClass('active prev next');
+
+        $('.viewer .album-image.next').attr('src', album_images[n].src);
+        $('.viewer .album-image.active').attr('src', album_images[a].src);
+        $('.viewer .album-image.prev').attr('src', album_images[p].src);
+
+        $('.viewer .album-image.prev').click(() => active = (active + num - 1) % num);
+        $('.viewer .album-image.next').click(() => active = (active + 1) % num);
+
+        $('.viewer .album-image.prev, .viewer .album-image.next').click((e) => {
+
+            $('.viewer .album-image').unbind();
+
+            prev = (active + num - 1) % num;
+            next = (active + 1) % num;
+
+            init_carousel(prev, active, next);
+        })
+    }
 
     function slider() {
 
-        $('.scroller .image-card').click((e) => {
-            $('.scroller .image-card').unbind();
+        $('.scroller .album').click((e) => {
+            $('.scroller .album').unbind();
 
-            $('.scroller .image-card.active').removeClass('active');
+            $('.scroller .album.active').removeClass('active');
             $(e.currentTarget).addClass('active');
-            $('.viewer .active-img').attr('src', $(e.currentTarget).attr('src'));
+
+            album_images = {}
+            $('.scroller .album.active .image-card').each((i, el) => {
+                album_images[i] = el;
+            });
+
+            num = Object.keys(album_images).length;
+            prev = num - 1;
+            active = 0;
+            next = 1;
 
             slider();
+            init_carousel(prev, active, next);
+        })
+    }
+
+    function pull_up() {
+        $('.arrow-icon').click((e) => {
+            // console.log(e);
+            $('.scroller').toggleClass('hide');
+            $('.arrow-icon').toggleClass('hide');
+
         })
     }
 
