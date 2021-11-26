890ba26a097cd86e910f9b32eaf7db1b4fa9a0bc
Rishikesh Devanathan <rishi.devanathan@gmail.com>
2021-11-26 18:42:17 +0530

    Documented gallery page html and css

--- gallery.php
+++ gallery.php
@@ -6,23 +6,27 @@
 
     <!-- preconnecting to google fonts to increase loading speed-->
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-
+    
     <!-- all third-party script imports -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+    <script
+        src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous">
     </script> <!-- jquery -->
 
     <!-- all custom imports -->
     <?php include getcwd() . '/partials/head.php' ?>
 
     <link rel="stylesheet" href="src/css/gallery.min.css">
-
 </head>
 
 <body>
 
+    <!-- Background cubes for design -->
     <img src="./src/assets/images/cubes.svg" class="cubes top-right">
     <img src="./src/assets/images/cubes.svg" class="cubes bottom-left">
 
+    <!-- Header with logo and back-button -->
     <div class="header">
         <a class="back-button" href="./index.php">
             <img src="./src/assets/images/back-head-white.svg">
@@ -31,24 +35,30 @@
         <img src="./src/assets/images/logo1.png" class="logo">
     </div>
 
+    <!-- Viewer to display photos of the album -->
     <div class="viewer">
-
         <img class="album-image prev">
         <img class="album-image active">
         <img class="album-image next">
-
     </div>
 
-    <img src="./src/assets/images/back-head-white.svg" class="arrow-icon hide" href="./index.php">
+    <!-- Pull up arrow -->
+    <img src="./src/assets/images/back-head-white.svg" class="arrow-icon hide">
 
+    <!-- Album Cover Scroller -->
     <div class="scroller hide">
+
+        <!-- Active album will be displayed in the viewer -->
         <div class="album active">
+            <!-- Album cover is what is viewed in the scroller -->
             <img src="./src/assets/images/gallery-images/dc.jpg" class="image-card album-cover">
             <img src="./src/assets/images/gallery-images/gconvo.jpg" class="image-card">
             <img src="./src/assets/images/gallery-images/bconvo.jpg" class="image-card">
             <img src="./src/assets/images/gallery-images/sconvo.jpg" class="image-card">
+            <!-- Name of the album -->
             <h3 class="album-title">Graduation Ceremony</h3>
         </div>
+    
         <div class="album">
             <img src="./src/assets/images/gallery-images/cs.jpg" class="image-card album-cover">
             <img src="./src/assets/images/gallery-images/ee.jpg" class="image-card">
